209651effe3483bab160027e58a1b9abdea87cbe
bab21 <2016061@iiitdmj.ac.in>
2020-10-11 14:25:13 +0530

    UC3:add hotel with different weekday and weekend rates

--- src/main/java/com/capgemini/hotelreservation/Hotel.java
+++ src/main/java/com/capgemini/hotelreservation/Hotel.java
@@ -2,22 +2,31 @@ package com.capgemini.hotelreservation;
 
 public class Hotel {
 	private String hotelName;
-	private int hotelRateRegularCustomer;
+	private int hotelWeekDayRateRegularCustomer;
+	private int hotelWeekEndRateRegularCustomer;
 	
-	public Hotel(String hotelName,int hotelRateRegularCustomer) {
+	
+	public Hotel(String hotelName,int hotelWeekDayRateRegularCustomer,int hotelWeekEndRateRegularCustomer) {
 		this.hotelName=hotelName;
-		this.hotelRateRegularCustomer=hotelRateRegularCustomer;
+		this.hotelWeekDayRateRegularCustomer=hotelWeekDayRateRegularCustomer;
+		this.hotelWeekEndRateRegularCustomer=hotelWeekEndRateRegularCustomer;
 	}
 	public void setHotelName(String hotelName) {
 		this.hotelName=hotelName;
 	}
-	public void setHotelRateRegularCustomer(int hotelRateRegularCustomer) {
-		this.hotelRateRegularCustomer=hotelRateRegularCustomer;
+	public void setHotelWeekDayRateRegularCustomer(int hotelWeekDayRateRegularCustomer) {
+		this.hotelWeekDayRateRegularCustomer=hotelWeekDayRateRegularCustomer;
+	}
+	public void setHotelWeekEndRateRegularCustomer(int hotelWeekEndRateRegularCustomer) {
+		this.hotelWeekEndRateRegularCustomer=hotelWeekEndRateRegularCustomer;
 	}
 	public String getHotelName() {
 		return this.hotelName;
 	}
-	public int getHotelRateRegularCustomer() {
-		return this.hotelRateRegularCustomer;
+	public int getHotelWeekDayRateRegularCustomer() {
+		return this.hotelWeekDayRateRegularCustomer;
+	}
+	public int getHotelWeekEndRateRegularCustomer() {
+		return this.hotelWeekEndRateRegularCustomer;
 	}
 }
--- src/main/java/com/capgemini/hotelreservation/HotelUtility.java
+++ src/main/java/com/capgemini/hotelreservation/HotelUtility.java
@@ -6,17 +6,8 @@ import java.util.*;
 public class HotelUtility {
 	public static List<Hotel> hotelList=new ArrayList<>();
 	
-	public static void addHotel(String hotelName,int hotelRateRegularCustomer) {
-		Hotel hotel=new Hotel(hotelName,hotelRateRegularCustomer);
+	public static void addHotel(String hotelName,int hotelWeekDayRateRegularCustomer,int hotelWeekEndRateRegularCustomer) {
+		Hotel hotel=new Hotel(hotelName,hotelWeekDayRateRegularCustomer,hotelWeekEndRateRegularCustomer);
 		hotelList.add(hotel);
-	}
-	public static void getCheapestHotel(LocalDate startDate,LocalDate endDate) {
-		Hotel cheapestHotel = hotelList.stream()  
-			                .min((hotel1, hotel2)->   
-			                hotel1.getHotelRateRegularCustomer() > hotel2.getHotelRateRegularCustomer() ? 1: -1).get();
-		
-		endDate=endDate.plusDays(1);
-		int numDays = Period.between(startDate, endDate).getDays();
-		System.out.println("Hotel Name of cheapest hotel: "+cheapestHotel.getHotelName()+" and cost is "+numDays*cheapestHotel.getHotelRateRegularCustomer());
 	}
 }
