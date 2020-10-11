538cf61c1a35ffb16b44d2f3901106125f10992f
bab21 <2016061@iiitdmj.ac.in>
2020-10-11 14:07:22 +0530

    UC2:cheapest Hotel for regular customer implemented

--- src/main/java/com/capgemini/hotelreservation/HotelReservation.java
+++ src/main/java/com/capgemini/hotelreservation/HotelReservation.java
@@ -1,4 +1,5 @@
 package com.capgemini.hotelreservation;
+import java.time.LocalDate;
 import java.util.*;
 
 public class HotelReservation {
@@ -9,7 +10,7 @@ public class HotelReservation {
 		do {
 			System.out.println("Enter choice\n"
 					         + "1.Add Hotel\n"
-					         + "2.Exit");
+					         + "2.Stop adding hotels");
 			choice=s.nextInt();
 			if(choice==1) {
 				System.out.println("Enter Hotel Name");
@@ -20,5 +21,22 @@ public class HotelReservation {
 			}
 			
 		}while(choice==1);
+		
+		
+		do{
+			System.out.println("Enter choice\n"
+			         + "1.Get Cheapest Hotel for a range of date\n"
+			         + "2.Exit");
+	        choice=s.nextInt();
+			if(choice==1) {
+				System.out.println("Enter start date(YEAR-MONTH-DAY)");
+				LocalDate startDate=LocalDate.parse(s.next());
+				System.out.println("Enter end date(YEAR-MONTH-DAY)");
+				LocalDate endDate=LocalDate.parse(s.next());
+				HotelUtility.getCheapestHotel(startDate, endDate);
+			}
+			
+		}while(choice==1);	
+		
 	}
 }
--- src/main/java/com/capgemini/hotelreservation/HotelUtility.java
+++ src/main/java/com/capgemini/hotelreservation/HotelUtility.java
@@ -1,4 +1,6 @@
 package com.capgemini.hotelreservation;
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.*;
 
 public class HotelUtility {
@@ -7,5 +9,14 @@ public class HotelUtility {
 	public static void addHotel(String hotelName,int hotelRateRegularCustomer) {
 		Hotel hotel=new Hotel(hotelName,hotelRateRegularCustomer);
 		hotelList.add(hotel);
+	}
+	public static void getCheapestHotel(LocalDate startDate,LocalDate endDate) {
+		Hotel cheapestHotel = hotelList.stream()  
+			                .min((hotel1, hotel2)->   
+			                hotel1.getHotelRateRegularCustomer() > hotel2.getHotelRateRegularCustomer() ? 1: -1).get();
+		
+		endDate=endDate.plusDays(1);
+		int numDays = Period.between(startDate, endDate).getDays();
+		System.out.println("Hotel Name of cheapest hotel: "+cheapestHotel.getHotelName()+" and cost is "+numDays*cheapestHotel.getHotelRateRegularCustomer());
 	}
 }
